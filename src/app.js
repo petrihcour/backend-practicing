4ec5641187c4e62c5beeddf4cae0c81e248a7b75
Vanessa <zdperfectx@gmail.com>
2023-11-09 15:36:14 -0600

    installed morgan package. added middleware to app.js. called on middleware function and morgan to log in console

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 const express = require("express"); // requiring the express package and assigning it to a variable
+const morgan = require("morgan");
 const app = express(); // the express package exports an Express function that i will assign to a new variable. i get a new express application and assign it to a VARIABLE
+
+const sayHello = (req, res, next) => {
+    res.send("Hello!");
+}
+
+app.use(morgan("dev"));
+app.use(sayHello);
+
+
 
 module.exports = app; // export the express function so it can be used in the server.js file 
