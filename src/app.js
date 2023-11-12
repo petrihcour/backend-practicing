d3d350dabed9259e9ee296438b433fe255ada8d2
Vanessa <zdperfectx@gmail.com>
2023-11-12 14:06:27 -0600

    utilized query and route parameters. to simplify code, removed functions and added anonymous functions into the routes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,34 @@
 const express = require("express"); // requiring the express package and assigning it to a variable
 const morgan = require("morgan");
 const app = express(); // the express package exports an Express function that i will assign to a new variable. i get a new express application and assign it to a VARIABLE
-
-const sayHello = (req, res, next) => {
-    res.send("Hello!");
-}
-
 app.use(morgan("dev"));
 
-//i can write it like this also
-// app.get("/hello", (req, res, next) => {
-//     res.send("Hellooooo!")
-// });
+app.get("/hello", (req, res) => {
+    console.log(req.query);
+    const name = req.query.name;
+    const content = name ? `Hello, ${name}!` : "Hello!";
+    res.send(content);
+});
 
-app.get("/hello", sayHello);
+app.get("/songs", (req, res) => {
+    const title = req.query.title;
+    res.send(title);
+})
+
+// Route parameters
+
+// You want this one to go before /say/:greeting because the routes could be confused for one another. if we request /say/goodbye first, since Express looks at each piece of middleware in order, it will request /say/goodbye first, thus displaying "Sorry to see you go!"" on the screen if  /say/goodbye is the path
+app.get("/say/goodbye", (req, res) => {
+    res.send("Sorry to see you go!")
+})
+
+
+app.get("/say/:greeting", (req, res, next) => {
+    const greeting = req.params.greeting;
+    const name = req.query.name;
+    const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
+    res.send(content);
+});
 
 
 module.exports = app; // export the express function so it can be used in the server.js file 
